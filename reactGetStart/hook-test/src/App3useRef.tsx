cbade89f5b7363776b86bd63b2a1481abb1530a3
yuting2.liu <yuting2.liu@ly.com>
2024-06-05 11:29:35 +0800

    feat: useRef

--- reactGetStart/hook-test/src/App3useRef.tsx
+++ reactGetStart/hook-test/src/App3useRef.tsx
@@ -1,20 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 /**
  * inputRef
+ * useRef 一般是用来存一些不是用于渲染的内容的
  */
 
+// function App() {
+//   const inputRef = useRef<HTMLInputElement>(null)
+//   useEffect(() => {
+//     inputRef?.current?.focus()
+//   })
+//   return (
+//     <div>
+//       <input ref={inputRef} />
+//     </div>
+//   );
+// }
+declare global {
+  interface Window {
+    iii: React.MutableRefObject<number>;
+  }
+}
+
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null)
-  useEffect(() => {
-    inputRef?.current?.focus()
-  })
+  const inputRef = useRef<number>(0)
+  const [, forceRender] = useState(0)
+  window.iii = inputRef
   return (
     <div>
-      <input ref={inputRef} />
+      {/* 点击inputRef.current的值发生改变了，但不会触发重新渲染，页面一直显示 0 */}
+      <div onClick={ () => {
+        inputRef.current += 1
+        // 每次点击因为setState 了，触发重新渲染
+        forceRender(Math.random())
+      }}>{inputRef.current}</div>
     </div>
   );
 }
